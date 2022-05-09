cfba45b8fac1778675f7f4615616d55527a323c3
Acer <Acer@Lishanth>
2022-05-10 00:10:05 +0530

    first commit

---
+++ DepartmentSpringBootJPAProject/src/main/java/com/example/demo/service/DepartmentServiceImpl.java
@@ -0,0 +1,20 @@
+package com.example.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Department;
+import com.example.demo.repository.DepartmentRepository;
+
+@Service
+public class DepartmentServiceImpl implements DepartmentService{
+	@Autowired
+	DepartmentRepository departmentrepo;
+
+	@Override
+	public Department saveDepartment(Department department) {
+		// TODO Auto-generated method stub
+		return departmentrepo.save(department);
+	}
+
+}
